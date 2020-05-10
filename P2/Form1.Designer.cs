fe4e9444d4dd601c9f500059001ef05cea030817
Bogdan Chisalescu <bogdan.chisalescu@gmail.com>
2020-05-10 18:36:42 +0300

    Tested PORTD and set some labels in Forms app

--- P2/Form1.Designer.cs
+++ P2/Form1.Designer.cs
@@ -28,13 +28,52 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Titlu = new System.Windows.Forms.Label();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Titlu
+            // 
+            this.Titlu.AutoSize = true;
+            this.Titlu.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Titlu.Location = new System.Drawing.Point(291, 9);
+            this.Titlu.Name = "Titlu";
+            this.Titlu.Size = new System.Drawing.Size(646, 39);
+            this.Titlu.TabIndex = 0;
+            this.Titlu.Text = "Minisistem pentru irigat plante - Proiect 2";
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.statusLabel.Location = new System.Drawing.Point(12, 84);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(87, 24);
+            this.statusLabel.TabIndex = 1;
+            this.statusLabel.Text = "Stare uC:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.Titlu);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Minisistem pentru irigat plante";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label Titlu;
+        private System.Windows.Forms.Label statusLabel;
     }
 }
 
--- P2micro/P2micro/main.c
+++ P2micro/P2micro/main.c
@@ -1,22 +1,16 @@
-/*
- * P2micro.c
- *
- * Created: 5/3/2020 12:01:14
- * Author : Bogdan
- */ 
-
 #include <avr/io.h>
 #include <util/delay.h>
 
 int main(void)
 {
-    /* Replace with your application code */
-	DDRC = 0xFF; //Set PORTC as output
+    //DDRD -  Port D Data Direction Register
+	//for each port pin, 1 = write, 0 = read
+	DDRD = 0x01; //Set PORTD, pin 0 as output
     while (1) 
     {
-		PORTC = 0xFF; //Make all pins on port C 5V
+		PORTD = 0x01; //Make pin 0 on port D 5V
 		_delay_ms(300); //Wait for 1 second
-		PORTC = 0x00; //Make all pins on port C 0V
+		PORTD = 0x00; //Make all pins on port D 0V
 		_delay_ms(300); //Wait again for 1 second
     }
 }
